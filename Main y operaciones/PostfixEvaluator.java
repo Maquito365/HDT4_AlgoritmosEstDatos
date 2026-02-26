de4a126289714fe341db79d28351319f3e9373d3
Maquito365 <macsoloj@gmail.com>
2026-02-26 16:39:33 -0600

    Se completo la clase que evalua si los token son numeros u operadores y hace las operaciones en base a que operador se escoja y se probo utilizando el main

--- Main y operaciones/PostfixEvaluator.java
+++ Main y operaciones/PostfixEvaluator.java
@@ -1,3 +1,36 @@
 public class PostfixEvaluator {
+    public int evaluate(String postfix, Stack<Integer> stack){
+        String[] tokens = postfix.split(" ");
+        for(String token : tokens){
+            if(Character.isDigit(token.charAt(0))){
+                stack.push(Integer.parseInt(token));
+            }else if(isOperator(token.charAt(0))){
+                int b = stack.pop();
+                int a = stack.pop();
+                int result = applyOperator(a, b, token.charAt((0)));
+                stack.push(result);
+            }
+        }
+        return stack.pop();
     
+    }
+
+    private boolean isOperator(char c){
+            return c== '+' || c == '-' || c == '*' || c == '/';
+    }
+
+    private int applyOperator(int a, int b, char operator){
+        switch(operator){
+            case '+':
+                return a+b;
+            case '-':
+                return a-b;
+            case '*':
+                return a*b;
+            case '/':
+                return a/b;
+            default:
+                throw new IllegalArgumentException("Operador no válido");
+        }
+    }
 }
