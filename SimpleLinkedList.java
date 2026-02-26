3eb700522f016cdf345ce57c332f18a5371a3c43
Maquito365 <macsoloj@gmail.com>
2026-02-25 18:22:40 -0600

    Clase de Nodo realizada para que pueda ser usada tanto por la SimpleLinkedListe como la Double además ya estan los metodos de la Lista Enlaza Simple

--- SimpleLinkedList.java
+++ SimpleLinkedList.java
@@ -1,3 +1,42 @@
-public class SimpleLinkedList {
+public class SimpleLinkedList<T> extends AbstractList<T> {
+    
+    Node<T> head;
+
+    public SimpleLinkedList(){
+        this.head = null;
+    }
+
+    @Override
+    public void addFirst(T element){
+        Node<T> newNode = new Node<>(element);
+        if(isEmpty()){
+             head = newNode;
+        } else{
+            newNode.setNext(head);
+            head = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public T removeFirst(){
+        if(isEmpty()){
+            throw new RuntimeException("La lista está vacia");
+        }else{
+        T data = head.getData();
+        head = head.getNext();
+        size--;
+        return data;
+        }
+    }
+
+    @Override
+    public T getFirst(){
+        if(isEmpty()){
+            throw new RuntimeException("La lista está vacía");
+        }else{
+            return head.getData();
+        }
+    }
     
 }
