9493a369abda5eecab0007ca66c11fb5d9a2b9d7
Maquito365 <macsoloj@gmail.com>
2026-02-25 19:01:49 -0600

    Se completó el ADT de las listas y se empezo a desarrollar los metodos de ListaStack para empezar el ADT de Stack

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -1,3 +1,51 @@
-public class DoubleLinkedList {
+public class DoubleLinkedList<T> extends AbstractList<T> {
+
+    Node<T> head;
+    Node<T> tail;
+
+    public DoubleLinkedList(){
+        this.head = null;
+        this.tail = null;  
+    }
+
+    @Override
+    public void addFirst(T element){
+        Node<T> newNode = new Node<>(element);
+        if(isEmpty()){
+            head = newNode;
+            tail = newNode;
+        } else{
+            newNode.setNext(head);
+            head.setPrevious(newNode);
+            head = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public T removeFirst(){
+        if(isEmpty()){
+            throw new RuntimeException("La Lista esta vacía");
+        }
+        T data = head.getData();
+        head = head.getNext();
+        if(head != null){
+            head.setPrevious(null);
+        } else {
+            tail = null;
+        }
+        size--;
+        return data;
+    }
+
+    @Override
+    public T getFirst(){
+        if(isEmpty()){
+            throw new RuntimeException("La lista está vacía");
+        }else{
+            return head.getData();
+        }
+    }
     
+
 }
--- ListaStack.java
+++ ListaStack.java
@@ -1,3 +1,33 @@
-public class ListaStack {
-    
+public class ListaStack<T> extends AbstractStack<T> {
+
+    private ListInterfaz<T> list;
+
+    public ListaStack(ListInterfaz<T> list){
+        this.list = list;
+    }
+
+    @Override
+    public void push(T element){
+        list.addFirst(element);
+        size++;
+    }
+
+    @Override
+    public T pop(){
+        if(isEmpty()){
+            throw new RuntimeException("La pila está vacia");
+        }else{
+            return list.removeFirst();
+        }
+    }
+
+    @Override
+    public T peek(){
+        if(isEmpty()){
+            throw new RuntimeException("La pila esta vacia");
+        }else{
+            size--;
+            return list.getFirst();
+        }
+    }
 }
