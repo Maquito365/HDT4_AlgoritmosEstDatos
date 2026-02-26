b02300c529a53484aab4c6db508f38aaad0d93e9
Maquito365 <macsoloj@gmail.com>
2026-02-25 19:09:22 -0600

    Se termino el ADT del Stack

--- ArrayListStack.java
+++ ArrayListStack.java
@@ -1,3 +1,35 @@
-public class ArrayListStack {
-    
+import java.util.ArrayList;
+
+public class ArrayListStack<T> extends AbstractStack<T> {
+
+    private ArrayList<T> list;
+
+    public ArrayListStack() {
+        this.list = new ArrayList<>();
+    }
+
+    @Override
+    public void push(T element){
+        list.add(element);
+        size++;
+    }
+
+    @Override
+    public T pop(){
+        if(isEmpty()){
+            throw new RuntimeException("La pila está vacia");
+        }else{
+            size--;
+            return list.remove(size);
+        }
+    }
+
+    @Override
+    public T peek(){
+        if(isEmpty()){
+            throw new RuntimeException("La pila esta vacia");
+        }else{
+            return list.get(size - 1);
+        }
+    }
 }
--- VectorStack.java
+++ VectorStack.java
@@ -1,3 +1,37 @@
-public class VectorStack {
-    
+import java.util.Vector;
+public class VectorStack<T> extends AbstractStack<T>{
+
+    private Vector<T> vector;
+
+    public VectorStack() {
+        this.vector = new Vector<>();
+    }
+
+    @Override
+    public void push(T element){
+        vector.add(element);
+        size++;
+    }
+
+    @Override
+    public T pop(){
+        if(isEmpty()){
+            throw new RuntimeException("La pila está vacia");
+        }else{
+            size--;
+            return vector.remove(size);
+        }
+    }
+
+    @Override
+    public T peek(){
+        if(isEmpty()){
+            throw new RuntimeException("La pila esta vacia");
+        }else{
+            return vector.get(size - 1);
+        }
+    }
 }
+ 
+    
+
