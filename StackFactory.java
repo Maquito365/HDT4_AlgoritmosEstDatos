e3ceefdfe3adf537be4c6a7147de419a16fd8c53
Maquito365 <macsoloj@gmail.com>
2026-02-25 19:18:15 -0600

    StackFactory hecha

--- StackFactory.java
+++ StackFactory.java
@@ -1,3 +1,21 @@
 public class StackFactory {
-    
+    public static <T> Stack<T> createStack(int optionStack, int optionList){
+
+        switch (optionStack){
+            case 1: 
+                return new ArrayListStack<>();
+            case 2:
+                return new VectorStack<>();
+            case 3:
+                if(optionList == 1){
+                    return new ListaStack<>(new SimpleLinkedList<>());
+                }else if (optionList == 2){
+                    return new ListaStack<>(new DoubleLinkedList<>());
+                }else{
+                    throw new IllegalArgumentException("Tipo de lista no valido");
+                }
+            default:
+                throw new IllegalArgumentException("Tipo de pila no valido");
+        }
+    }
 }
